cd19c9785bffef16c81401ce0ae899d463b5ab96
Simon Joseph <sjoseph@gerberfinance.com>
2012-10-25 18:13:36 -0400

    init

--- app/assets/javascripts/calc.js
+++ app/assets/javascripts/calc.js
@@ -3,7 +3,7 @@
 function PopupCalc(pageURL, title,w,h) {
 var left = (screen.width/2)-(w/2);
 var top = (screen.height/2)-(h/2);
-var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
 } 
 
 
