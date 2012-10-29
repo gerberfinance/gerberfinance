12da7aec0fa24b6761fcaf2504eb7fa22ad9b125
Simon Joseph <sjoseph@gerberfinance.com>
2012-10-29 15:08:45 -0400

    init

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -11,7 +11,7 @@ class ContactController < ApplicationController
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver
       NotificationsMailer.new_request(@message).deliver
-	  render :text => '<script type="text/javascript"> window.close() </script>'
+	  render :text => '<script type="text/javascript"> window.parent.TINY.box.hide() </script>'
     else
       flash.now.alert = "Please fill all fields."
       render :new, :layout =>false
