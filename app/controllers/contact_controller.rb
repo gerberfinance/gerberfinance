87c47c07c47704f276670a1f0e3da5c14cbf40b9
Simon Joseph <sjoseph@gerberfinance.com>
2012-10-29 14:48:57 -0400

    init

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -11,7 +11,7 @@ class ContactController < ApplicationController
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver
       NotificationsMailer.new_request(@message).deliver
-      redirect_to(calc_path :alert => "Message was successfully sent.")
+      :alert => "Message was successfully sent."
     else
       flash.now.alert = "Please fill all fields."
       render :new, :layout =>false
