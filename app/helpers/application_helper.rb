3c4259bc71b7aef1942eadb642082d27710ef141
Simon Joseph <sjoseph@gerberfinance.com>
2012-11-08 09:09:09 -0500

    init

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+# Request from an iPhone or iPod touch? (Mobile Safari user agent)
+def iphone_user_agent?
+  request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
+end
 end
