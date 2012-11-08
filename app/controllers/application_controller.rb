3e8c3bf87f6c0939563c08a84a80a031e63e669b
Simon Joseph <sjoseph@gerberfinance.com>
2012-11-08 09:15:43 -0500

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :adjust_format_for_iphone
+
+private
+  # Set iPhone format if request to iphone.gerberfinance.com
+  def adjust_format_for_iphone
+    request.format = :iphone if iphone_request?
+  end
+  # Return true for requests to iphone.gerberfinance.com
+  def iphone_request?
+    return (request.subdomains.first == "iphone" || params[:format] == "iphone")
+  end
+end
 end
